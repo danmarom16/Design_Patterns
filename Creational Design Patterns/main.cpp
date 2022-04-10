f57060d85bdaddd44c35e6b1de22c8e31a38f49c
danmarom16 <92876036+danmarom16@users.noreply.github.com>
2022-04-10 21:49:06 +0300

    Factory

--- Creational Design Patterns/main.cpp
+++ Creational Design Patterns/main.cpp
@@ -1,167 +1,131 @@
 #include <iostream>
-#include "vector"
+#include "string"
+#include "map"
+#include "typeinfo"
 using namespace std;
+// This is a combination of Factory Design Pattern And O(1) Access using user input
+// to the matching object using it's creator
 
-
-class Platform{
+/**
+ * This is the Product interface.
+ */
+class GymDumbbell {
 private:
-    int x;
-    int y;
-    int l;
+    int kg;
+    int price;
 public:
-    Platform(int x, int y, int l) : x(x), y(y), l(l){};
-    int getX(){return this->x;}
-    int getY(){return this->y;}
-    int getLength(){return this->l;}
-};
-
-class Enemy{
-private:
-    int x;
-    int y;
-public:
-    Enemy(int x, int y) : x(x), y(y){};
-    int getX(){return this->x;}
-    int getY(){return this->y;}
-};
-
-class Character{
-private:
-    int x;
-    int y;
-public:
-    Character(int x, int y) : x(x), y(x){};
-    int getX(){return this->x;}
-    int getY(){return this->y;}
-};
-
-
-class LevelGraphics{
-public:
-    virtual void paintPlatform(Platform* p) = 0;
-    virtual void paintCharacter(Character* e) = 0;
-    virtual void paintEnemy(Enemy* p) = 0;
-};
-
-class AladdinGraphics : public LevelGraphics{
-    void paintPlatform(Platform* p) override{
-        cout << "Platform at position x:" + to_string(p->getX()) + " y:" + to_string(p->getY()) +
-        " with length:" + to_string(p->getLength()) + " Painted Aladdin Style!" << endl;
-    }
-    void paintCharacter(Character* c) override {
-        cout << "Character at position x:" + to_string(c->getX()) + " y:" + to_string(c->getY()) + " Painted Aladdin Style!" << endl;
-    }
-    void paintEnemy(Enemy* e) override {
-        cout << "Enemy at position x:" + to_string(e->getX()) + " y:" + to_string(e->getY()) + " Painted Aladdin Style!" << endl;
-    }
-};
-
-class MarioGraphics : public LevelGraphics{
-
-    void paintPlatform(Platform* p) override{
-        cout << "Platform at position x:" + to_string(p->getX()) + " y:" + to_string(p->getY()) +
-                " with length:" + to_string(p->getLength()) + " Painted Mario Style!" << endl;
-    }
-    void paintCharacter(Character* c) override {
-        cout << "Character at position x:" + to_string(c->getX()) + " y:" + to_string(c->getY()) + " Painted Mario Style!" << endl;
-    }
-    void paintEnemy(Enemy* e) override {
-        cout << "Enemy at position x:" + to_string(e->getX()) + " y:" + to_string(e->getY()) + " Painted Mario Style!" << endl;
-    }
-};
-
-class LevelDirector{
-protected:
-    vector<Platform*> platforms;
-    vector<Enemy*> enemies;
-    Character* character;
-public:
-    LevelDirector() = default;
-    void createPlatform(int x_pos, int y_pos, int length) {
-        platforms.push_back(new Platform(x_pos, y_pos, length));
-    }
-    void createCharacter(int x_pos, int y_pos){
-        character = new Character(x_pos, y_pos);
-    }
-
-    void createEnemy(int x_pos, int y_pos) {
-        enemies.push_back(new Enemy(x_pos,y_pos));
-    }
-    /**
-     * This Method Constructs a level and paints it using dynamically given LevelGraphics.
-     * @param lg
-     */
-    virtual void constructLevel(LevelGraphics* lg) = 0;
-};
-
-class BeginnerLevel : public LevelDirector{
-
-    void constructLevel(LevelGraphics *lg) override{
-
-        // Object Creation.
-        int i = 0;
-        for(i; i < 5; i++){
-            createPlatform(i,i,i);
-        }
-        for(i = 0; i < 5; i++){
-            createEnemy(i,i);
-        }
-        createCharacter(i,i);
-
-
-        // Object Painting
-        for(i = 0; i < 5; i++){
-            lg->paintPlatform(this->platforms[i]);
-        }
-        for(i = 0; i < 5; i++){
-            lg->paintEnemy(this->enemies[i]);
-        }
-        lg->paintCharacter(character);
-    }
-};
-
-class AdvancedLevel : public LevelDirector{
-
-    void constructLevel(LevelGraphics *lg) override{
-
-        // Object Creation.
-        int i = 0;
-        for(i; i < 10; i++){
-            createPlatform(i,i,i);
-        }
-        for(i = 0; i < 10; i++){
-            createEnemy(i,i);
-        }
-        createCharacter(i,i);
-
-
-        // Object Painting
-        for(i = 0; i < 10; i++){
-            lg->paintPlatform(this->platforms[i]);
-        }
-        for(i = 0; i < 10; i++){
-            lg->paintEnemy(this->enemies[i]);
-        }
-        lg->paintCharacter(character);
-    }
+    GymDumbbell(int kg, int p) : kg(kg), price(p){};
+    int getPrice(){return this->price;};
+    int getDes(){return this->kg;};
 };
 
 /**
- * Main using Builder Design Pattern and in runtime inject all possible Combinations.
+ * Product Inherit Classes.
  */
+class FiveKgDumbbell : public GymDumbbell {
+public:
+    FiveKgDumbbell() : GymDumbbell(5, 100){};
+};
+
+class TenKgDumbbell : public GymDumbbell {
+public:
+    TenKgDumbbell() : GymDumbbell(10, 120){};
+};
+
+class TwentyKgDumbbell : public GymDumbbell {
+public:
+    TwentyKgDumbbell() : GymDumbbell(20, 200){};
+};
+
+/**
+ * The Creator Factory Class
+ */
+class GymDumbbellCreatorFactory{
+
+private:
+    /**
+     * This is the Creator interface.
+     */
+    class GymDumbbellCreator {
+    public:
+        GymDumbbellCreator() = default;
+        virtual GymDumbbell* create() = 0;
+    };
+
+    /**
+    * Creator inherit Classes.
+    */
+    class FiveKgDumbbellCreator : public GymDumbbellCreator {
+    public:
+        FiveKgDumbbellCreator() = default;
+        ~FiveKgDumbbellCreator() = default;
+        GymDumbbell* create() override{
+            cout << "Five Kg Dumbbell Was Created!"<< endl;
+            return new FiveKgDumbbell();
+        }
+    };
+
+    class TenKgDumbbellCreator : public GymDumbbellCreator {
+    public:
+        TenKgDumbbellCreator() = default;
+        ~TenKgDumbbellCreator() = default;
+        GymDumbbell* create() override{
+            cout << "Ten Kg Dumbbell Was Created!"<< endl;
+            return new TenKgDumbbell();
+        }
+    };
+
+    class TwentyKgDumbbellCreator : public GymDumbbellCreator {
+    public:
+        TwentyKgDumbbellCreator() = default;
+        ~TwentyKgDumbbellCreator() = default;
+        GymDumbbell* create() override{
+            cout << "Twenty Kg Dumbbell Was Created!"<< endl;
+            return new TwentyKgDumbbell();
+        }
+    };
+
+    map <string, GymDumbbellCreator*> dumbbellsCreator;
+public:
+    /**
+     * @param type is the string representing the user object to create.
+     * @return new GymDumbbell.
+     *
+     * This is the most important function in this complex structure.
+     * It takes the user string, checks map for exsistance of the string.
+     * If exist - gets a matching run time dumbbell Creator, and invokes
+     * it's create function -> which returns new GymDumbbell injected Dynamically.
+     */
+    GymDumbbell* createDumbbell(string type){
+        // if not found - return iterator to end element.
+        GymDumbbellCreator* dumbbellCreator = this->dumbbellsCreator.find(type)->second;
+        if(dumbbellCreator != dumbbellsCreator.end()->second ||
+                ((dumbbellCreator == dumbbellsCreator.end()->second) &&
+                        (type == dumbbellsCreator.end()->first))){
+            return dumbbellCreator->create();
+        }
+        return nullptr;
+    }
+    GymDumbbellCreatorFactory(){
+        this->dumbbellsCreator["Five"] = new FiveKgDumbbellCreator();
+        this->dumbbellsCreator["Ten"] = new TenKgDumbbellCreator();
+        this->dumbbellsCreator["Twenty"] = new TwentyKgDumbbellCreator();
+    };
+    ~GymDumbbellCreatorFactory() = default;
+};
+
+
 int main() {
-    LevelGraphics* aladdin = new AladdinGraphics();
-    LevelGraphics* mario = new MarioGraphics();
+    GymDumbbellCreatorFactory creatorFactory;
+    string dumbbellType;
+    cin >> dumbbellType;
 
-    LevelDirector* beginner = new BeginnerLevel();
-    LevelDirector* advanced = new AdvancedLevel();
-
-    cout << "Beginner Levels:" << endl;
-    beginner->constructLevel(aladdin);
-    beginner->constructLevel(mario);
-    cout << "---------------------------------------------------" << endl;
-    cout << "Advanced Levels:" << endl;
-    advanced->constructLevel(aladdin);
-    advanced->constructLevel(mario);
+    /*
+     * The most amazing thing in this design pattern is this line.
+     * Instead of ugly SOLID breaker switch case which check dynamic type,
+     * We just pass the input to the GymDumbbellCreatorFactory which creates
+     * the matching dumbbell in a SOLID way.
+     */
+    GymDumbbell* dumbbell = creatorFactory.createDumbbell(dumbbellType);
     return 0;
 }
